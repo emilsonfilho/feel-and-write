3f7a5b04dc77f2a7b33ff9e532b29e570e4d39da
Emilson Filho <emilsonfilhocontato@gmail.com>
2023-11-24 23:35:43 -0300

    Formatando código

--- pages/Layout/Modal/index.js
+++ pages/Layout/Modal/index.js
@@ -3,23 +3,23 @@
  * @param {string} idOpenModalBtn - Says the id of the button to open the modal
  */
 function activeModal(idOpenModalBtn) {
-    document.addEventListener('DOMContentLoaded', () => {
-        const openModalBtn = document.getElementById(idOpenModalBtn);
-        const closeModalBtn = document.getElementsByClassName('close')[0];
-        const modal = document.getElementById('modal');
+  document.addEventListener("DOMContentLoaded", () => {
+    const openModalBtn = document.getElementById(idOpenModalBtn);
+    const closeModalBtn = document.getElementsByClassName("close")[0];
+    const modal = document.getElementById("modal");
 
-        openModalBtn.addEventListener('click', () => {
-            modal.style.display = 'block'
-        })
+    openModalBtn.addEventListener("click", () => {
+      modal.style.display = "block";
+    });
 
-        closeModalBtn.addEventListener('click', () => {
-            modal.style.display = 'none'
-        })
+    closeModalBtn.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
 
-        window.addEventListener('click', event => {
-            if (event.target === modal) {
-                modal.style.display = 'none'
-            }
-        })
-    })
+    window.addEventListener("click", (event) => {
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    });
+  });
 }
